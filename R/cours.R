a80a9b8bb534fd4667c528c1e40983e870771b1c
jumentib <jumentib@eduroam-112030.grenet.fr>
2019-03-27 15:52:01 +0100

    select2

---
+++ R/cours.R
@@ -0,0 +1,2 @@
+# licence tjrs GPL-3
+# usethis::use_package(package) --> pour mettre les imports des packages
--- R/test.R
+++ R/test.R
@@ -10,3 +10,25 @@
 test <- function() {
   print("AAAAAH")
 }
+
+
+#' select2
+#' 
+#' A function to select column in a data.frame
+#'
+#' @param data a data.frame
+#' @param selection the column to select, can be the a vector of interger 
+#' or a vector of the names of the column
+#'
+#' @return the choosen column
+#' @export
+#'
+#' @examples
+#' x <- data.frame(x1 = rnorm(10), x2 = rnorm(10))
+#' 
+#' select2(x, "x1")
+#' select2(x, 1)
+select2 <- function(data, selection) {
+  data[selection]
+}
+
